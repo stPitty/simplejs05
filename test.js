d502f1774cd655c48c13f346fb5ea98260831aaf
Petr Stankin <stanoke@mail.ru>
2022-04-13 13:07:14 +0300

    test deleted

--- test.js
+++
@@ -1,7 +0,0 @@
-const date = new Date(null);
-date.setSeconds(50);
-let result = date.toISOString().substr(11, 8);
-
-console.log(result)
-result = date.setSeconds(45).toString().slice(11, 19)
-console.log(result)
