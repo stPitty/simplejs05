c2dc1d8658b1542f877cae1fde70ae1ca545e265
Petr Stankin <stanoke@mail.ru>
2022-04-13 21:31:26 +0300

    tasks changed

--- countdown/task.js
+++ countdown/task.js
@@ -3,26 +3,21 @@ const download = window.document.getElementById('download');
 
 download.href = 'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png'
 
-let seconds = span.textContent;
+let seconds = span.textContent - 58;
 span.textContent = setDate(seconds)
 
 function setDate(seconds) {
   return new Date(seconds * 1000).toISOString().substr(11, 8);
 }
 
-function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
-
-(async () => {
-  while (seconds > 0) {
-    await sleep(1000);
+setInterval(() => {
+  if (seconds > 0) {
     seconds--;
-    span.textContent = setDate(seconds)
+    span.textContent = setDate(seconds);
+  } else if (seconds === 0) {
+    seconds = -1;
+    alert('Вы победили в конкурсе');
+    download.click();
   }
-  alert('Вы победили в конкурсе')
-  download.click()
-})();
+}, 1000);
 
--- mole-game/task.js
+++ mole-game/task.js
@@ -1,7 +1,6 @@
 const holeGame = window.document.getElementsByClassName('hole-game')[0];
 const dead = window.document.getElementById('dead');
 console.log(holeGame)
-let score = 0
 
 for (let hole of holeGame.children) {
   hole.onclick = () => {
@@ -9,7 +8,7 @@ for (let hole of holeGame.children) {
       dead.textContent++
     } else {
       alert(`Игра завершена! Ваш счет: ${dead.textContent}`);
-      window.document.location.reload()
+      dead.textContent = 0;
     }
   }
 }
